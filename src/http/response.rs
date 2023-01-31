fcede027b7aa77c009c0b56c23391e9f39860fd6
Henry Davies <Henry Davies>
2023-01-31 11:04:14 +0000

    Implement safe request handler

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,3 +1,5 @@
+use std::io::{Write, Result as IoResult};
+
 use super::status_code::StatusCode;
 
 #[derive(Debug)]
@@ -9,5 +11,13 @@ pub struct Response {
 impl Response {
     pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
         Response { status_code, body }
+    }
+
+    pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
+        let body = match &self.body {
+            Some(b) => b,
+            None => "",
+        };
+        write!(stream, "HTTP/1.1 {} {}\r\n\r\n{}", self.status_code, self.status_code.reason_phrase(), body)
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,17 @@
+use crate::http::{Response, StatusCode, ParseError};
 use std::net::TcpListener;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::convert::TryFrom;
-
 use crate::http::Request;
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+
+    fn handle_bad_request(&mut self, e:  &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
 
 pub struct Server {
     addr: String,
@@ -15,7 +24,7 @@ impl Server {
         }
     }
 
-    pub fn run(self) {
+    pub fn run(self, mut handler: impl Handler) {
         println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
@@ -28,12 +37,18 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
-                                    dbg!(request);
+                                    handler.handle_request(&request)
                                 },
-                                Err(e) => println!("Failed to parse request: {}", e),
+                                Err(e) => {
+                                    handler.handle_bad_request(&e)
+                                },
                             };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
+                            }
                         },
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
